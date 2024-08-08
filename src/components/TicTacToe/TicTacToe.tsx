5e6942ad886e8d3d0f793e5ebbbfc97aa7b0b8c3
Wojciech Łukowski <wojtek304@gmail.com>
2024-08-09 01:52:21 +0200

    Update displayed game status depending on game state

--- src/components/TicTacToe/TicTacToe.tsx
+++ src/components/TicTacToe/TicTacToe.tsx
@@ -1,11 +1,22 @@
+import { useMachine } from '@xstate/react';
 import { GameStatus } from '../GameStatus';
 import { GameBoard } from '../GameBoard';
 import { GameForm } from '../GameForm';
+import { ticTacToeMachine } from '../../machines/ticTacToeMachine';
 
 const TicTacToe = () => {
+  const [state] = useMachine(ticTacToeMachine);
+
+  const { winner, player } = state.context;
+
   return (
     <div data-testid="tic-tac-toe">
-      <GameStatus>game state goes here</GameStatus>
+      <GameStatus>
+        {state.matches('idle') && "Select grid size and let's begin!"}
+        {state.matches('playing') && `Next player: ${player ? 'x' : 'o'}`}
+        {state.matches('won') && `Winner: ${winner}`}
+        {state.matches('draw') && `Draw`}
+      </GameStatus>
       <GameBoard size={3} />
       <GameForm />
     </div>
