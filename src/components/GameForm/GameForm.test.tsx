55cf405f70489ca6312b20be0e4688e7bf2dfd72
Wojciech Łukowski <wojtek304@gmail.com>
2024-08-09 02:39:11 +0200

    Update GameForm tests

--- src/components/GameForm/GameForm.test.tsx
+++ src/components/GameForm/GameForm.test.tsx
@@ -1,9 +1,30 @@
 import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 import GameForm from './GameForm';
 
 describe('GameForm', () => {
-  it('renders form', () => {
-    render(<GameForm />);
-    expect(screen.getByRole('form')).toBeVisible();
+  it('renders accessible form', async () => {
+    const startGame = jest.fn();
+    const restartGame = jest.fn();
+    const user = userEvent.setup();
+
+    const { rerender } = render(
+      <GameForm isStartable={true} startGame={startGame} restartGame={restartGame} />,
+    );
+
+    const button = screen.getByRole('button');
+
+    await user.click(button);
+    expect(startGame).toHaveBeenCalledWith('3');
+    expect(startGame).toHaveBeenCalledTimes(1);
+
+    await user.type(screen.getByRole('spinbutton'), '3');
+    expect(startGame).toHaveBeenCalledWith('33');
+    expect(startGame).toHaveBeenCalledTimes(2);
+
+    rerender(<GameForm isStartable={false} startGame={startGame} restartGame={restartGame} />);
+
+    await user.click(button);
+    expect(restartGame).toHaveBeenCalledTimes(1);
   });
 });
